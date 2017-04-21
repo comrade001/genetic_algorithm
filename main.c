212f145c3d2d2614a7d49a3df6be6ee879010d92
comrade001 <abrahamsa@live.com.mx>
2017-04-20 21:21:09 -0500

    Función CrearIndividuo implementada

--- main.c
+++ main.c
@@ -17,17 +17,41 @@ typedef struct  {
 
 const unsigned int Numero_de_Genes=2;
 const unsigned int Numero_de_Individuos=5;
-INDIVIDUO Poblacion[Numero_de_Individuos];
+
 void InicializarPoblacion(const unsigned int NumeroGenes, unsigned int BitsGen);
 
 
 int main()
 {
     printf("Hello world!\n");
-    return 0;
+	//INDIVIDUO *pInd;
+	//pInd=CrearIndividuo(Numero_de_Genes,10);
+	//INDIVIDUO Poblacion[Numero_de_Individuos];
+	return 0;
 }
 
 void InicializarPoblacion(const unsigned int NumeroGenes, unsigned int BitsGen)
 {
 
 }
+
+INDIVIDUO* CrearIndividuo(const unsigned int Numero_de_Genes, const unsigned int BitsGen)
+{
+	INDIVIDUO *pInd;
+	/*Asignar mememoria a la estructura Individuo*/
+	pInd=(INDIVIDUO*)malloc(sizeof(INDIVIDUO));
+
+	if(pInd == NULL)
+		printf("Error en memoria\n");
+
+	pInd->cromosoma=(unsigned char*)malloc(sizeof(unsigned char)*Numero_de_Genes*BitsGen);
+	*pInd->BitPorGen=BitsGen;
+	pInd->valor=(float*)malloc(sizeof(float)*Numero_de_Genes);	
+
+	return(pInd);
+}
+
+
+
+
+
